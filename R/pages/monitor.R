da95b3e1309776809bc2a20b85ac32fd9f63592c
Schröder <Eike.Schroeder@stifterverband.de>
2023-11-11 17:00:01 +0100

    without debugging

--- R/pages/monitor.R
+++ R/pages/monitor.R
@@ -1,3 +1,6 @@
+#' Missing description
+#' @noRd
+
 module_monitor_ui <- function(id = "monitor", label = "m_monitor", type = "all") {
   ns <- NS(id)
   tagList(
@@ -6,8 +9,8 @@ module_monitor_ui <- function(id = "monitor", label = "m_monitor", type = "all")
       titlePanel(paste("Monitor", "!")),
       div(
         id = ns("mon"),
-        uiOutput(ns("monitor_svg")),
-        verbatimTextOutput(ns("debug"))
+        uiOutput(ns("monitor_svg")) # ,
+        # verbatimTextOutput(ns("debug"))
       ),
       tags$script(
         HTML(
@@ -24,7 +27,8 @@ module_monitor_ui <- function(id = "monitor", label = "m_monitor", type = "all")
   )
 }
 
-
+#' Missing description
+#' @noRd
 
 module_monitor_server <- function(id = "monitor", con, type = "all") {
   moduleServer(
@@ -34,7 +38,7 @@ module_monitor_server <- function(id = "monitor", con, type = "all") {
 
       observeEvent(input$mon, { print(input$mon) })
 
-      output$debug <- renderPrint(input$mon)
+      # output$debug <- renderPrint(input$mon)
 
       output$monitor_svg <- renderUI({
         HTML(
@@ -44,58 +48,3 @@ module_monitor_server <- function(id = "monitor", con, type = "all") {
     }
   )
 }
-
-
-
-
-#
-# module_monitor_ui <- function(id = "monitor", label = "m_monitor", type = "all") {
-#   ns <- NS(id)
-#   tagList(
-#     fluidPage(
-#       shinyjs::useShinyjs(),
-#       tags$script(
-#         HTML(
-#           paste0(
-#             "document.addEventListener('click', function(e) {
-#               if (e.target.closest('svg')) {
-#                 var elemId = e.target.id;
-#                 var elemClass = e.target.className.baseVal;
-#
-#                 console.log('Geklickte Element-ID:', elemId);
-#                 console.log('Geklickte Element-Klasse:', elemClass);
-#
-#                 if (typeof Shiny !== 'undefined') {
-#                   Shiny.setInputValue('", ns("clickedElementId"), "', elemId, {priority: 'event'});
-#                   Shiny.setInputValue('", ns("clickedElementClass"), "', elemClass, {priority: 'event'});
-#                 }
-#               }
-#             });"
-#           )
-#         )
-#       ),
-#       titlePanel(paste("Monitor", "!")),
-#       HTML(
-#         readLines("www/img/Test_Monitor.svg")
-#       )
-#     )
-#   )
-# }
-#
-# module_monitor_server <- function(id = "monitor", con, type = "all") {
-#   moduleServer(
-#     id,
-#     function(input, output, session) {
-#       ns <- session$ns
-#
-#       observeEvent(input[[ns('clickedElementId')]], {
-#         print(paste("Geklickte Element-ID:", input[[ns('clickedElementId')]]))
-#       })
-#
-#       observeEvent(input[[ns('clickedElementClass')]], {
-#         print(paste("Geklickte Element-Klasse:", input[[ns('clickedElementClass')]]))
-#       })
-#
-#     }
-#   )
-# }
